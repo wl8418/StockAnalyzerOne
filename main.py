809ea00bca86eff370470588492ee46f2b9979a1
wl8418 <179896078+wl8418@users.noreply.github.com>
2026-01-03 16:09:56 -0600

    Can graph close prices of multiple stocks now.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # The bank stocks whose performance I am interested in
-tickers = ["USB", "TFC", "PNC", "MTB", "FITB"]
+tickers = ["USB", "TFC", "PNC", "MTB", "FITB","KRE"] #KRE is the ticker for a regional bank ETF which I am using to see
+                                                    #the impact of the 2023 banking crisis
 # Download historical data
 data = yf.download(tickers, start="2023-03-01", end="2023-03-15")
 
@@ -16,11 +17,13 @@ for ticker in tickers:
     stock = yf.Ticker(ticker)
     info = stock.info
     print(f"{ticker}: {info.get('longName', 'Not found')} | Sector: {info.get('sector', 'N/A')}")
-usb_close = data["Close"]["USB"]
+# Plot closing prices for all banks
+closes = data.xs("Close", level=0, axis=1)
 
-usb_close.plot(figsize=(12, 6))
-plt.title("USB Closing Prices")
+# Easy plotting
+closes.plot(figsize=(12, 6))
+plt.title("Regional Bank Closing Prices")
 plt.ylabel("Price ($)")
-plt.xlabel("Date")
 plt.grid(True, alpha=0.3)
 plt.show()
+
